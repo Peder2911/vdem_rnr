378aadd63505fdfe4df8c8b4698e1b833dfd5a47
Peder Landsverk <pglandsverk@gmail.com>
2019-12-17 13:58:37 +0100

    Added documentation to the models.R file
    Also cleaned up the code a little

--- models.R
+++ models.R
@@ -10,38 +10,65 @@ shh(library(glue))
 shh(library(tictoc))
 shh(library(parallel))
 
+shh(library(yaml))
+
 tic("Runtime")
-
-memoize <- function(name,call){
-   if(name %in% list.files("Cache")){
-      cat(paste0("\x1b[33mUsing cache for ",name,"\x1b[0m\n"))
-      readRDS(paste0("Cache/",name))
-   } else {
-      cat(paste0("\x1b[32mRunning and caching ",name,"\x1b[0m\n"))
-      res <- eval(call)
-      saveRDS(res,paste0("Cache/",name))
-      res
-   }
-}
-
 tic("Reading data")
 dat <- readRDS("data.rds")
 partial <- filter(dat,year >= 1946)
 toc()
 
 # ================================================
+# This script trains 12 models with varying specs,
+# four for each of the following groups:
+#
+# 1) Partial data (1946-2016), regular models
+# 2) Partial data (1946-2016), year-fixed effects models 
+# 3) Full data (1900-2016), regular models 
+#
+# The model objects are written to cache for further 
+# processing (eg. writeout with stargazer).
+#
+
+# ================================================
 # CONSTANTS ======================================
 # ================================================
 
-DEPENDENT_VARIABLE <- "c2_onset"
+# The constants are defined in a configuration file.
+# this makes it possible to re-use this script
+# with several different variable configurations.
+# 
+# Using the same script for the different configurations 
+# ensures that nothing else than what one intends
+# to vary, varies between the specifications. 
 
-INDEP_ONE <- "lfree_fair_elections"
-INDEP_TWO <- "lhorizontal_constraint_narrow"
+# Configs written so far:
 
-BASE_CONTROLS <- c("llnpop200", "llnGDPPerCapita200", "decay_c_term_long")
-NEW_CONTROLS <- c("ethfrac", "lmtnest", "anyNbConflict")
+# tab1_overview.yaml: Reproduction of results and addition of
+# new control variables. 
 
-RANDOM_EFFECTS <- c("(1|gwno)")
+# tab1_overview_major.yaml: Same as the previous,
+# only without regarding minor conflicts in any
+# of the variables.
+
+a <- commandArgs(trailingOnly = TRUE)
+if("--config" %in% a){
+   fname <- a[which(a == "--config")+1]
+} else {
+   fname <- "Config/tab1_overview.yaml"
+}
+
+config <- yaml.load_file(fname)
+
+DEPENDENT_VARIABLE <- config$dependent_variable
+
+INDEP_ONE <- config$indep_one 
+INDEP_TWO <- config$indep_two 
+
+BASE_CONTROLS <- config$base_controls 
+NEW_CONTROLS <- config$new_controls 
+
+RANDOM_EFFECTS <-  config$random_effects 
 
 # ================================================
 # REPRODUCTION ===================================
@@ -56,6 +83,13 @@ modelOnset <- function(rhs){
    makeFormula(DEPENDENT_VARIABLE,rhs)
 }
 
+# ================================================
+# These are the four "variants" in each group:
+# There are two different variables of interest,
+# indep. one and indep. two. In addition, two 
+# sets of control variables are tested.
+#
+
 formulae <- lapply(list(
    c(INDEP_ONE,BASE_CONTROLS),
    c(INDEP_ONE,BASE_CONTROLS,NEW_CONTROLS),
@@ -64,6 +98,11 @@ formulae <- lapply(list(
    ), modelOnset)
 
 tic("Fitting models")
+
+# ================================================
+# Here different modelling procedures are applied
+# to the formulae:
+#
 
 models_partial_base <- formulae %>%
    lapply(function(f) {
@@ -90,14 +129,8 @@ models_full_base <- formulae %>%
    lapply(function(f) {
       glm(f,data = dat, family = binomial("logit"))
    })
+
 saveRDS(models_full_base,"Cache/models_full_base.rds")
-
-toc()
-
-tic("Stargazer")
-stargazer(models_partial_base, models_partial_re, models_full_base, type = "html") %>%
-   writeLines("/tmp/tab.html")
-
 toc()
 
 toc()
