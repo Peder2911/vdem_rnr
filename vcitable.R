7c5dad9e593f9e753dcde32bcd4fd404f47072ee
Peder Landsverk <pglandsverk@gmail.com>
2020-01-06 11:51:31 +0100

    Added VCI index testing table
    Added a table that shows coefficients given for different specifications
    of the VCI index (Vertical constraints).

---
+++ vcitable.R
@@ -0,0 +1,42 @@
+
+library(timelib)
+library(texreg)
+library(parallel)
+library(yaml)
+
+source("functions.R")
+
+data <- readRDS("Cache/prepped_data.rds")
+VARNAMES <- yaml.load_file("vnames.yaml")
+
+indexVariables<-c(
+   "v2x_suffr",
+   "FHKN_frefair",
+   "v2x_elecoff",
+   "v2x_frassoc_thick",
+   "v2x_freexp_thick"
+   )
+
+
+
+models <- mclapply(c(indexVariables,""), function(ivar){
+   variables <- indexVariables[which(!indexVariables == ivar)]
+   data$index <- apply(data[variables],1,function(r){
+      purrr::reduce(r,`*`)
+   })
+   data$index <- offset(data$index)
+
+   mdl <- glm(c2_onset ~ index + llnpop200 + llnGDPPerCapita200 + timesince + timesince_sq + timesince_cb + ethfrac + lmtnest + nbConflict,
+      family = binomial("logit"),
+      data = data[data$year >= 1946,])
+   mdl
+}, mc.cores = 7)
+
+
+texreg(models,
+   custom.coef.map = VARNAMES,
+   custom.model.names = c(paste0(
+      "Sans ", indexVariables
+   ), "All")) %>%
+   stripenv() %>%
+   writeLines("Out/index_testing.tex")
