cb88909501b85d4248a2a0764fa1049b86341211
peder2911 <pglandsverk@gmail.com>
2020-03-09 15:49:23 +0100

    Refactored simulations
    Changed the simulation code. Now it outputs the plots properly.

--- scripts/sim.R
+++ scripts/sim.R
@@ -62,60 +62,62 @@ margPlotSet <- function(simdata, origdata, name){
 saveSet <- function(...,name){
    set <- margPlotSet(...,name = name)
    set$nrow <- 1
-   ggsave(glue("/tmp/v/{name}"),do.call(grid.arrange,set),device = "png",
-      height = 4, width = 8)
-}
+      ggsave(glue("Out/{name}"),do.call(grid.arrange,set),device = "pdf",
+         height = 4, width = 8)
+   }
 
 
 # =%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%=%
 
-dat <- readRDS("Cache/prepped_data.rds")
-partial <- dat[dat$year >= 1946,]
+   dat <- readRDS("Cache/prepped_data.rds")
+   partial <- dat[dat$year >= 1946,]
 
-models <- c(
-   "t3pol_model_6.rds",
-   "t3_model_7.rds",
-   "t3pol_model_7.rds",
-   "t3_model_6.rds"
-)
+   models <- c(
+      "t3pol_model_6.rds",
+      "t3_model_7.rds",
+      "t3pol_model_7.rds",
+      "t3_model_6.rds"
+   )
 
-for (model in models) {
-   simfile <- paste0(model,".simulations.csv")
-   simfilePath <- file.path("Cache",simfile)
-   if(simfile %in% list.files("Cache")){
-      writeLines(glue("Using cache for {model} simulations"))
-      simset <- read.csv(simfilePath, stringsAsFactors = FALSE)
-   } else {
-      writeLines(glue("Caching {model} simulations"))
-      m <- readRDS(file.path("Cache",model))
+   for (model in models) {
+      simfile <- paste0(model,".simulations.csv")
+      simfilePath <- file.path("Cache",simfile)
+      if(simfile %in% list.files("Cache")){
+         writeLines(glue("Using cache for {model} simulations"))
+         simset <- read.csv(simfilePath, stringsAsFactors = FALSE)
+      } else {
+         writeLines(glue("Caching {model} simulations"))
+         m <- readRDS(file.path("Cache",model))
 
-      xvar <- "lhorizontal_constraint_narrow"
-      yvar <- "lfree_fair_elections"
+         xvar <- "lhorizontal_constraint_narrow"
+         yvar <- "lfree_fair_elections"
 
-      variables <- list()
-      variables[[xvar]] <- seq(0,1,length.out=RES+1)
-      variables[[yvar]] <- seq(0,1,length.out=RES+1) 
+         variables <- list()
+         variables[[xvar]] <- seq(0,1,length.out=RES+1)
+         variables[[yvar]] <- seq(0,1,length.out=RES+1) 
 
 
-      simset <- makeTestSet(partial,variables,mean)
+         simset <- makeTestSet(partial,variables,mean)
 
-      simset[[paste(xvar,yvar,sep=":")]] <- simset[[xvar]] * simset[[yvar]] 
-      simset[[paste(yvar,xvar,sep=":")]] <- simset[[xvar]] * simset[[yvar]] 
-      simset[[paste0(xvar,"_sq")]] <- simset[[xvar]] ^ 2
-      simset[[paste0(yvar,"_sq")]] <- simset[[yvar]] ^ 2
+         simset[[paste(xvar,yvar,sep=":")]] <- simset[[xvar]] * simset[[yvar]] 
+         simset[[paste(yvar,xvar,sep=":")]] <- simset[[xvar]] * simset[[yvar]] 
+         simset[[paste0(xvar,"_sq")]] <- simset[[xvar]] ^ 2
+         simset[[paste0(yvar,"_sq")]] <- simset[[yvar]] ^ 2
 
-      simset$timesince_sq <- simset$timesince ^ 2
-      simset$timesince_cb <- simset$timesince ^ 3
-      simset$x_polity_sq <- simset$x_polity ^ 2
+         simset$timesince_sq <- simset$timesince ^ 2
+         simset$timesince_cb <- simset$timesince ^ 3
+         simset$x_polity_sq <- simset$x_polity ^ 2
 
-      simset <- cbind(simset,sim(simset,m,vcov = countryCl))
-      simset$sim_mean <- logitToProb(simset$sim_mean)
-      simset$sim_upper <- logitToProb(simset$sim_upper)
-      simset$sim_lower <- logitToProb(simset$sim_lower)
+         simset <- cbind(simset,sim(simset,m,vcov = countryCl))
+         simset$sim_mean <- logitToProb(simset$sim_mean)
+         simset$sim_upper <- logitToProb(simset$sim_upper)
+         simset$sim_lower <- logitToProb(simset$sim_lower)
 
-      write.csv(simset,simfilePath,row.names = FALSE)
+         write.csv(simset,simfilePath,row.names = FALSE)
+      }
+      modelname <- gsub("\\..*$","",model)
+      writeLines(glue("Saving {modelname}"))
+      saveSet(simset,partial,name=paste0(modelname,".pdf"))
    }
-   saveSet(simset,partial,name=paste0(model,".png"))
-}
 
-writeLines("Done!")
+   writeLines("Done!")
